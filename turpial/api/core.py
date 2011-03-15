187188cc3dac0424c4e8ac51221836f693a722dd
Wil Alvarez <wil_alejandro@yahoo.com>
2011-03-15 10:37:32 -0430

    Added register_account method to core

--- turpial/api/core.py
+++ turpial/api/core.py
@@ -26,32 +26,43 @@ class Core:
         self.log.debug('Started')
         self.accman = AccountManager()
         
-    def login(self):
-        response = Response()
-        for aid, acc in self.accman:
-            self.log.debug('Authenticating with %s' % aid)
-            try:
-                response.add(acc.auth())
-            except Exception, exc:
-                print traceback.print_exc()
-                self.log.debug('Authentication Error')
-                return Response(666)
-        return response
+    def register_account(self, username, password, protocol_id):
+        self.log.debug('Registering account %s' % username)
+        self.accman.register(username, password, protocol_id)
         
-    def get_column_statuses(self, account_id, column_id, count=STATUSPP):
+    def login(self, acc_id):
+        self.log.debug('Authenticating with %s' % acc_id)
         try:
-            account = self.accman.get(account_id)
-            if column_id == ColumnType.TIMELINE:
+            account = self.accman.get(acc_id)
+            return Response(account.auth())
+        except Exception, exc:
+            print traceback.print_exc()
+            self.log.debug('Authentication Error')
+            return Response(code=401)
+        
+    def get_column_statuses(self, acc_id, col_id, count=STATUSPP):
+        try:
+            account = self.accman.get(acc_id)
+            if col_id == ColumnType.TIMELINE:
                 rtn = account.get_timeline(count)
-            elif column_id == ColumnType.REPLIES:
+            elif col_id == ColumnType.REPLIES:
                 rtn = account.get_replies(count)
-            elif column_id == ColumnType.DIRECTS:
+            elif col_id == ColumnType.DIRECTS:
                 rtn = account.get_directs(count)
-            elif column_id == ColumnType.SENT:
+            elif col_id == ColumnType.SENT:
                 rtn = account.get_sent(count)
-            elif column_id == ColumnType.FAVORITES:
+            elif col_id == ColumnType.FAVORITES:
                 rtn = account.get_favorites(count)
-            
             return Response(rtn)
         except KeyError:
-            return Response(666)
+            return Response(code=410)
+    
+    def update_status(self, acc_id, text, in_reply_id=None):
+        try:
+            account = self.accman.get(acc_id)
+            return Response(account.update_status(text, in_reply_id))
+        except Exception, exc:
+            print traceback.print_exc()
+            self.log.debug('Error updating status')
+            return Response(code=999)
+            
