ad9f98ad3cb54f60629867a1d0d08455f770fd69
satanas <wil_alejandro@yahoo.com>
2011-10-08 01:25:53 -0300

    Improved common module. Added new 'tools' submodule

---
+++ libturpial/common/__init__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# Global values/constants for Turpial
+#
+# Author: Wil Alvarez (aka Satanas)
+# Oct 07, 2011
+
+VERSION = '0.6.8-a1'
+STATUSPP = 20
+
+class ProtocolType:
+    TWITTER = 'twitter'
+    IDENTICA = 'identica'
+
+class UpdateType:
+    DM = 'dm'
+    STD = 'std'
+    PROFILE = 'profile'
+
+class ColumnType:
+    TIMELINE = 'timeline'
+    REPLIES = 'replies'
+    DIRECTS = 'directs'
+    SENT = 'sent'
+    FAVORITES = 'favorites'
+
+ERROR_CODES = {
+    100: "",
+    304: "There was no new data to return",
+    401: "Invalid or missing credentials",
+    404: "Invalid request",
+    406: "What are you trying to search?",
+    420: "Wait! Your search is being too intense",
+    
+    500: "Oops! The server is broken",
+    502: "Oh oh... The server is down",
+    503: "The server is overloaded",
+    
+    801: "There are some network issues",
+    802: "Your status was sent. Don't try again",
+    803: "User is already a friend",
+    804: "You've already requested to follow that user",
+    805: "Invalid account",
+    806: "That column doesn't exist",
+    807: "Server is not responding",
+    808: "Accounr already logged in",
+    
+    900: "Feature not implemented yet",
+    999: "Unknown error",
+}
+
---
+++ libturpial/common/tools.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Tools for Turpial
+#
+# Author: Wil Alvarez (aka Satanas)
+# Oct 07, 2011
+
+import os
+import sys
+
+OS_LINUX = 'linux'
+OS_WINDOWS = 'windows'
+OS_MAC = 'darwin'
+OS_JAVA = 'java'
+OS_UNKNOWN = 'unknown'
+
+def clean_bytecodes(root_path, logger=None):
+    """Clean all .pyc y .pyo. This is intented to use by all UI interfaces"""
+    if logger:
+        logger.debug("Cleaning da house...")
+    
+    path = os.path.join(os.path.dirname(root_path))
+    i = 0
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            path = os.path.join(root, f)
+            if path.endswith('.pyc') or path.endswith('.pyo'): 
+                if logger:
+                    logger.debug("Deleting %s" % path)
+                os.remove(path)
+    if logger:
+        logger.debug("Everything is clean now")
+        
+def detect_os():
+    """ Returns a string according to the OS host """
+    if sys.platform.startswith('linux'):
+        return OS_LINUX
+    elif sys.platform.startswith('win32'):
+        return OS_WINDOWS
+    elif sys.platform.startswith('darwin'):
+        return OS_MAC
+    elif sys.platform.startswith('java'):
+        return OS_JAVA
+    else:
+        return OS_UNKNOWN
