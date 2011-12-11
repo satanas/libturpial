eb4168f97c4be3cbaa4019160943bd3a142fa378
satanas <wil.alejandro@gmail.com>
2011-12-11 16:19:39 -0300

    Added support for media entities in twitter

--- libturpial/api/protocols/twitter/twitter.py
+++ libturpial/api/protocols/twitter/twitter.py
@@ -220,6 +220,11 @@ class Main(Protocol):
                 entities['urls'].append(Entity(expanded_url, display_url, 
                     url['url']))
             
+            for url in tweet['entities']['media']:
+                display_url = 'http://' + url['display_url']
+                entities['urls'].append(Entity(url['media_url'], display_url, 
+                    url['url']))
+                
             for ht in tweet['entities']['hashtags']:
                 text = '#' + ht['text']
                 url = "%s%s" % (self.urls['hashtags'], ht['text'])
