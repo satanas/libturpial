de27ac6c1f3eed7362cb51cd5e828d123b920f17
satanas <wil.alejandro@gmail.com>
2012-01-07 16:18:27 -0300

    Added exceptions module (forgotten)

---
+++ libturpial/common/exceptions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+""" Module to handle custom exceptions for Turpial """
+#
+# Author: Wil Alvarez (aka Satanas)
+# Jan 07, 2012
+
+class URLShortenError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+class NoURLException(Exception):
+    def __init__(self):
+        self.message = 'No URL to short'
+
+class UploadImageError(Exception):
+    def __init__(self, message):
+        self.message = message
