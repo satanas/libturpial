dfa77b75090227feb9fbcc15a5f828b088a65aed
4ndreaSt4gi <stagi.andrea@gmail.com>
2012-03-09 15:44:43 +0100

    Typos fixed

--- libturpial/api/services/showmedia/servicelist.py
+++ libturpial/api/services/showmedia/servicelist.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-"""A list containing all upload pic services"""
+"""A list containing all show media services"""
 #
 # Author: Andrea Stagi (aka 4ndreaSt4gi)
-# 2012-03-08
+# 2012-08-01
 
 from twitpic import TwitpicMediaContent
 
