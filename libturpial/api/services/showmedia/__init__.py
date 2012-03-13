6a30405bef3eb295658e417ff92f2e4c4c9218aa
4ndreaSt4gi <stagi.andrea@gmail.com>
2012-03-13 01:19:56 +0100

    Added foursquare mediacontent service

--- libturpial/api/services/showmedia/__init__.py
+++ libturpial/api/services/showmedia/__init__.py
@@ -10,11 +10,13 @@ from libturpial.api.services.showmedia.yfrog import YfrogMediaContent
 from libturpial.api.services.showmedia.instagram import InstagramMediaContent
 from libturpial.api.services.showmedia.pictwitter import PicTwitterMediaContent
 from libturpial.api.services.showmedia.youtube import YouTubeMediaContent
+from libturpial.api.services.showmedia.foursquare import FoursquareMediaContent
 
 SHOWMEDIA_SERVICES = {
     'twitpic': TwitpicMediaContent(),
     'yfrog': YfrogMediaContent(),
     'instagram': InstagramMediaContent(),
     'pic.twitter.com': PicTwitterMediaContent(),
-    'youtube' : YouTubeMediaContent()
+    'youtube' : YouTubeMediaContent(),
+    'foursquare' : FoursquareMediaContent()
 }
