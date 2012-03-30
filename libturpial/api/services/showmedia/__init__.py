daf3bd82490db7358f9ca401247f853dbba5d276
William Abrahan Cabrera Reyes <william@linux.es>
2012-03-30 14:13:41 -0430

    Added imgur service

--- libturpial/api/services/showmedia/__init__.py
+++ libturpial/api/services/showmedia/__init__.py
@@ -5,22 +5,24 @@
 # Author: Andrea Stagi (aka 4ndreaSt4gi)
 # 2012-08-01
 
+from libturpial.api.services.showmedia.imgur import ImgurMediaContent
+from libturpial.api.services.showmedia.pinterest import PinterestMediaContent
+from libturpial.api.services.showmedia.lockerz import LockerzMediaContent
 from libturpial.api.services.showmedia.twitpic import TwitpicMediaContent
 from libturpial.api.services.showmedia.yfrog import YfrogMediaContent
 from libturpial.api.services.showmedia.instagram import InstagramMediaContent
 from libturpial.api.services.showmedia.pictwitter import PicTwitterMediaContent
 from libturpial.api.services.showmedia.youtube import YouTubeMediaContent
 from libturpial.api.services.showmedia.foursquare import FoursquareMediaContent
-from libturpial.api.services.showmedia.pinterest import PinterestMediaContent
-from libturpial.api.services.showmedia.lockerz import LockerzMediaContent
 
 SHOWMEDIA_SERVICES = {
+    'imgur': ImgurMediaContent(),
+    'pinterest': PinterestMediaContent(),
+    'lockerz': LockerzMediaContent(),
     'twitpic': TwitpicMediaContent(),
     'yfrog': YfrogMediaContent(),
     'instagram': InstagramMediaContent(),
     'pic.twitter.com': PicTwitterMediaContent(),
     'youtube' : YouTubeMediaContent(),
     'foursquare' : FoursquareMediaContent()
-    'pinterest': PinterestMediaContent(),
-    'lockerz': LockerzMediaContent(),
 }
---
+++ libturpial/api/services/showmedia/imgur.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""Imgur show media content service"""
+#
+# Author: William Cabrera (aka willicab)
+# 2012-03-28
+
+import traceback
+#from showmediaservice import *
+from libturpial.api.models.mediacontent import *
+from libturpial.api.services.showmedia.base import *
+from libturpial.api.interfaces.service import ServiceResponse
+
+class ImgurMediaContent(ShowMediaService):
+    def __init__(self):
+        ShowMediaService.__init__(self)
+        self.url_pattern = "(http(s)?://)?imgur.com"
+        
+    def do_service(self, url):
+        try:
+            page_content = self._get_content_from_url(url)
+            media_content, nombre = self.__find_url_in_html(page_content)
+            rawimg = self._get_content_from_url(media_content)
+            return ServiceResponse(MediaContent(IMAGE_CONTENT, nombre, rawimg))
+        except Exception, error:
+            self.log.debug("Error: %s\n%s" % (error, traceback.print_exc()))
+            return ServiceResponse(err=True, err_msg=_('Problem showing media content'))
+
+    def __find_url_in_html(self, html):
+        pass
+        path = "<link rel=\"image_src\" href=\""
+        start = html.find(path) + len(path)
+        end = html.find('" />', start, len(html))
+        return html[start:end], html[start:end].split('/')[-1]
