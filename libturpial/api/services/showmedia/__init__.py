3709036d0fc75effe8fc8ea45681a457635b0e9d
satanas <wil.alejandro@gmail.com>
2012-09-30 14:48:29 -0300

    Added flic.kr support

--- libturpial/api/services/showmedia/__init__.py
+++ libturpial/api/services/showmedia/__init__.py
@@ -10,8 +10,9 @@ from libturpial.api.services.showmedia.lockerz import LockerzMediaContent
 from libturpial.api.services.showmedia.twitpic import TwitpicMediaContent
 from libturpial.api.services.showmedia.yfrog import YfrogMediaContent
 from libturpial.api.services.showmedia.instagram import InstagramMediaContent
-from libturpial.api.services.showmedia.viame import ViameMediaContent
 from libturpial.api.services.showmedia.pictwitter import PicTwitterMediaContent
+from libturpial.api.services.showmedia.viame import ViameMediaContent
+from libturpial.api.services.showmedia.flickr import FlickrMediaContent
 from libturpial.api.services.showmedia.youtube import YouTubeMediaContent
 from libturpial.api.services.showmedia.foursquare import FoursquareMediaContent
 
@@ -23,6 +24,7 @@ SHOWMEDIA_SERVICES = {
     'instagram': InstagramMediaContent(),
     'pic.twitter.com': PicTwitterMediaContent(),
     'via.me': ViameMediaContent(),
+    'flic.kr': FlickrMediaContent(),
 #    'youtube' : YouTubeMediaContent(),
 #    'foursquare' : FoursquareMediaContent()
 }
