c3df00ddf0ffcb87d23da0830eeb5c8decab322c
satanas <wil.alejandro@gmail.com>
2013-05-10 22:55:02 -0300

    Documented AccountManager

--- libturpial/api/managers/accountmanager.py
+++ libturpial/api/managers/accountmanager.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-import logging
-
 from libturpial.common import LoginStatus, build_account_id
 from libturpial.lib.config import AccountConfig
 from libturpial.api.models.account import Account
@@ -12,8 +10,6 @@ from libturpial.common.exceptions import ErrorCreatingAccount, \
 
 class AccountManager:
     def __init__(self, config):
-        self.log = logging.getLogger('AccountManager')
-        self.log.debug('Started')
         self.config = config
         self.__accounts = {}
 
@@ -29,47 +25,55 @@ class AccountManager:
         #self.protocol.timeout = timeout
 
         self.__accounts[account_id] = Account.load(account_id)
-        self.log.debug('Account %s loaded successfully' % account_id)
         return account_id
 
     def register_oauth_account(self, protocol_id, username, key, secret, verifier):
+        """
+        Register a new OAuth based account in libturpial record for the protocol
+        specified by *protocol_id*, *username* and OAuth params: *key*,
+        *secret* and *verifier*. Return the id of the new account if the
+        registration process was successful, otherwise raise an
+        :class:`libturpial.common.exceptions.ErrorCreatingAccount` exception.
+        """
         if username == '' or protocol_id == '':
             raise ErrorCreatingAccount
 
         account_id = build_account_id(username, protocol_id)
-        if account_id in self.__accounts:
-            self.log.debug('Account %s is already registered' % account_id)
-        else:
+        if account_id not in self.__accounts:
             account = Account.new_oauth(protocol_id, username, key, secret, verifier)
             self.__accounts[account_id] = account
-            self.log.debug('Account %s registered successfully' % account_id)
         return account_id
 
     def register_basic_account(self, protocol_id, username, password):
+        """
+        Register a new Basic HTTP based account in libturpial record for the
+        protocol specified by *protocol_id*, *username* and OAuth params: *key*,
+        *secret* and *verifier*. Return the id of the new account if the
+        registration process was successful, otherwise raise an
+        :class:`libturpial.common.exceptions.ErrorCreatingAccount` exception.
+        """
         if username == '' or protocol_id == '':
             raise ErrorCreatingAccount
 
         account_id = build_account_id(username, protocol_id)
-        if account_id in self.__accounts:
-            self.log.debug('Account %s is already registered' % account_id)
-        else:
+        if account_id not in self.__accounts:
             account = Account.new_basic(protocol_id, username, password)
             self.__accounts[account_id] = account
-            self.log.debug('Account %s registered successfully' % account_id)
         return account_id
 
     def unregister(self, account_id, delete_all):
+        """
+        Remove the account *account_id* from libturpial record. If *delete_all*
+        is **True** all configuration files are deleted from disk. Be careful
+        because this operation can not be undone.
+        """
         if account_id in self.__accounts:
             self.__accounts[account_id].remove(delete_all)
             del self.__accounts[account_id]
-        else:
-            self.log.debug('Account %s is not registered' % account_id)
 
-    def login_status(self, account_id, status):
+    def set_login_status(self, account_id, status):
         if account_id in self.__accounts:
             self.__accounts[account_id].logged_in = status
-        else:
-            self.log.debug('Account %s is not registered' % account_id)
 
     def get(self, account_id, validate_login=False):
         """
@@ -77,6 +81,9 @@ class AccountManager:
         associated to *account_id* if it has been loaded or try to load the
         account otherwise. If any of the previous method fails it raise an
         :class:`libturpial.common.exceptions.ErrorLoadingAccount` exception.
+        If *validate_login* is **True** and the account is not logged in a
+        :class:`libturpial.common.exceptions.AccountNotLoggedIn` exception is
+        raised.
         """
         try:
             account = self.__accounts[account_id]
