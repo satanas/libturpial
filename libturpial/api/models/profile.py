f8cc276352a1785d4c92a7d768cbbb683eb1663c
Bouska <bouska@eskapa.be>
2014-02-03 11:10:49 +0100

    [enh] Add entities attibut to Profile() class

--- libturpial/api/models/profile.py
+++ libturpial/api/models/profile.py
@@ -36,6 +36,7 @@ class Profile:
         self.location = ''
         self.url = ''
         self.bio = ''
+        self.entities = {}
         self.following = None
         self.followed_by = None
         self.follow_request = False
