de43e7a3e2050b6e9df85751b39788afadbccec7
satanas <wil.alejandro@gmail.com>
2014-02-15 22:27:57 -0300

    Finished list.py tests

---
+++ tests/models/test_list.py
@@ -0,0 +1,25 @@
+from libturpial.api.models.list import List
+
+class TestColumn:
+    @classmethod
+    def setup_class(self):
+        self.list_ = List()
+        self.list_.id_ = "foo-twitter-list"
+        self.list_.slug = "list"
+        self.list_.title = "My List"
+        self.list_.suscribers = []
+        self.list_.description = "Foo list"
+        self.list_.single_unit = "tweet"
+        self.list_.plural_unit = "tweets"
+
+    def test_structure(self):
+        assert self.list_.id_ == "foo-twitter-list"
+        assert self.list_.slug == "list"
+        assert self.list_.title == "My List"
+        assert self.list_.suscribers == []
+        assert self.list_.description == "Foo list"
+        assert self.list_.single_unit == "tweet"
+        assert self.list_.plural_unit == "tweets"
+
+    def test_repr(self):
+        assert str(self.list_) == "libturpial.api.models.List foo-twitter-list"
