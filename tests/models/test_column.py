4ebeb674f90d2759906764ab58f3a489f3b44ebc
satanas <wil.alejandro@gmail.com>
2014-02-15 22:27:35 -0300

    Finished column.py tests

---
+++ tests/models/test_column.py
@@ -0,0 +1,17 @@
+from libturpial.api.models.column import Column
+
+class TestColumn:
+    @classmethod
+    def setup_class(self):
+        self.column = Column("foo-twitter", "timeline")
+
+    def test_structure(self):
+        assert self.column.size == 0
+        assert self.column.id_ == "foo-twitter-timeline"
+        assert self.column.slug == "timeline"
+        assert self.column.updating == False
+        assert self.column.singular_unit == "tweet"
+        assert self.column.plural_unit == "tweets"
+
+    def test_repr(self):
+        assert str(self.column) == "libturpial.api.models.Column foo-twitter-timeline"
