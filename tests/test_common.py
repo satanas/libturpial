166c94621d6c0af4ca40e68028f1e11eb7c8ae10
satanas <wil.alejandro@gmail.com>
2014-02-27 11:12:06 -0300

    Added tests for tools

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,10 +1,15 @@
+import os
+import sys
 import pytest
+import logging
 
 from libturpial.common import *
+from libturpial.common.tools import *
 
 class TestCommon:
     @classmethod
-    def setup_class(self):
+    @pytest.fixture(autouse=True)
+    def setup_class(self, monkeypatch):
         self.account_id = "dummy-twitter"
 
     def test_get_username(self):
@@ -37,4 +42,48 @@ class TestCommon:
 
         preview = is_preview_service_supported("http://unknown.service.com/bla")
         assert not preview
+
+    def test_clean_bytecodes(self, monkeypatch):
+        path = os.path.join('..', __file__)
+        monkeypatch.setattr(os, 'remove', lambda x: None)
+
+        clean_bytecodes(path, logging)
+
+    def test_detect_os(self, monkeypatch):
+        monkeypatch.setattr(sys, 'platform', 'linux')
+        assert detect_os() == OS_LINUX
+
+        monkeypatch.setattr(sys, 'platform', 'win32')
+        assert detect_os() == OS_WINDOWS
+
+        monkeypatch.setattr(sys, 'platform', 'darwin')
+        assert detect_os() == OS_MAC
+
+        monkeypatch.setattr(sys, 'platform', 'java')
+        assert detect_os() == OS_JAVA
+
+        monkeypatch.setattr(sys, 'platform', 'foobar')
+        assert detect_os() == OS_UNKNOWN
+
+    def test_get_urls(self):
+        text = "Dumb test http://google.com with more than (www.foobar.com) one URL http://www.anotherexample.org."
+        urls = get_urls(text)
+        assert "http://google.com" in urls
+        assert "www.foobar.com" in urls
+        assert "http://www.anotherexample.org" in urls
+
+    def test_timestamp_to_localtime(self, monkeypatch):
+        class DummyTime:
+            def __init__(self, value=0):
+                self.tm_isdst = value
+
+        monkeypatch.setattr(time, 'timezone', 89)
+        monkeypatch.setattr(time, 'localtime', lambda: DummyTime(0))
+        localtime = timestamp_to_localtime(123456789)
+        assert localtime == 123456700
+
+        monkeypatch.setattr(time, 'altzone', 6000)
+        monkeypatch.setattr(time, 'localtime', lambda: DummyTime(1))
+        localtime = timestamp_to_localtime(123456789)
+        assert localtime == 123450789
 
